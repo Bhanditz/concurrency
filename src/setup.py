5970370c4a50e5aa9e0792eabf08ca81859421de
Drew Nutter <drew@drewnutter.com>
2018-07-02 16:28:03 -0400

    Add source files

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='concurrency',
+	version='0.1.0',
+	description='Assortment of concurrency related python tools',
+	url='https://github.com/dnut/concurrency',
+	author='Drew Nutter',
+	author_email='drew@drewnutter.com',
+	license='GPLv3',
+	packages=['concurrency'],
+	zip_safe=False,
+)
